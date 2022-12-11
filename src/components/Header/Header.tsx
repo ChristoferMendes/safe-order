83e0fbf4ad3ebe1f8d2bc41dedab11b3c8d54df5
ChristoferMendes <christoferluizdsm@gmail.com>
2022-12-11 08:15:25 -0300

    initial login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import {
+  View, StatusBar, HStack, IconButton, Icon, Text,
+} from 'native-base';
 
 export function Header() {
   return (
     <View>
-      <Text>Header</Text>
+      <StatusBar backgroundColor="#35c11c" barStyle="light-content" />
+      <HStack alignItems="center">
+        <IconButton icon={<Icon size="sm" as={MaterialIcons} name="menu" color="black" />} />
+        <Text>
+          Home
+        </Text>
+      </HStack>
+
     </View>
   );
 }
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 import { SignUp } from '../screens/SignUp';
 import Login from '../screens/Login';
 import { Home } from '../screens/Home';
@@ -6,11 +8,29 @@ import { Home } from '../screens/Home';
 const Stack = createNativeStackNavigator();
 
 export function StackNavigator() {
+  const [hasAuthToken, setHasAuthToken] = useState('');
+
+  const getTokenFromAsyncStorage = async () => {
+    const token = await AsyncStorage.getItem('@storage_token');
+    if (!token) return '';
+
+    return setHasAuthToken(token);
+  };
+
+  useEffect(() => {
+    getTokenFromAsyncStorage();
+  }, []);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      {hasAuthToken ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
